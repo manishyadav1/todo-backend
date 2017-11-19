e02f693a54a2b5f00c99a834829203e974f88ce3
manishyadav <yadav.ma@husky.neu.edu>
2017-11-19 01:14:24 -0500

    back end

---
+++ src/main/java/com/team29/plagiarismdetector/PlagiarismDetectorApplication.java
@@ -0,0 +1,12 @@
+package com.team29.plagiarismdetector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlagiarismDetectorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlagiarismDetectorApplication.class, args);
+	}
+}
